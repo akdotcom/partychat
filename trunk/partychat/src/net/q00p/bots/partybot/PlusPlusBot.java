d3097240d9166bf54964df2d74bcc9e024a4e65b
mihaiparparita <mihaiparparita@063ddc40-722a-11de-b54e-29fd3b018196>
2007-04-23 00:21:53 +0000

    - Add a /reasons command that lists all of the actions that led to a target   having the score that it does - Clean up /score so that it works with no arguments and always sorts the   targets by score - Remove support for single-letter scoring - Clean up increment/decrement response message formatting (no empty parentheses   when no reason was given)

--- trunk/partychat/src/net/q00p/bots/partybot/PlusPlusBot.java
+++ trunk/partychat/src/net/q00p/bots/partybot/PlusPlusBot.java
@@ -1,4 +1,4 @@
-//Copyright 2005 Google Inc. All Rights Reserved
+// Copyright 2005 Google Inc. All Rights Reserved
 
 package net.q00p.bots.partybot;
 
@@ -9,23 +9,24 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import net.q00p.bots.Message;
 
 /**
  * Simple plusplusbot implementation.
- * It stores the data in memory so all scores will be lost if the server is bounced -
- * this should be easily fixable once I decide to stop being lazy and do it "right".
  *
  * Created on Mar 25, 2006
  * @author <a href="mailto:dolapo@gmail.com">Dolapo Falola</a>
@@ -33,12 +34,18 @@ import net.q00p.bots.Message;
 public class PlusPlusBot {
   
   private static final String LOG_DELIMITER = "\t";
+  
+  private static final String REASON_FORMAT = 
+      " (%s)";
 
   private static final String INC_MESSAGE_FORMAT =
-    "woot! %s -> %d (%s)";
+    "woot! %s -> %d%s";
   
   private static final String DEC_MESSAGE_FORMAT =
-    "ouch! %s -> %d (%s)";
+    "ouch! %s -> %d%s";
+
+  private static final String OP_DESCRIPTION_FORMAT =
+    "  %s by %s%s";
   
   /**
    * Things that can't be ++d
@@ -48,13 +55,95 @@ public class PlusPlusBot {
   /**
    * chat -> {targetscore}
    */
-  private final Map<String, Map<String, Integer>> scoreBoard;
+  private final Map<String, Map<String, Score>> scoreBoard;
+  
+  private static class Score implements Comparable<Score> {
+    private final String target;
+    private final List<Op> ops = new LinkedList<Op>();
+    private int score = 0;
+    
+    public Score(String target) {
+      this.target = target;
+    }
+    
+    public Message increment(String from, String reason) {
+      return processOp(from, reason, true);
+    }
+    
+    public Message decrement(String from, String reason) {
+      return processOp(from, reason, false);
+    }
+    
+    public String getTarget() {
+      return target;
+    }
+    
+    public int getCurrentScore() {
+      return score;
+    }
+    
+    public int compareTo(Score other) {
+      return score - other.score;
+    }
+    
+    private Message processOp(String from, String reason, boolean isIncrement) {
+      score += isIncrement ? 1 : -1;
+      
+      Op op = new Op(this, from, reason, isIncrement);
+      ops.add(op);
+      
+      return op.getResponseMessage(); 
+    }
+    
+    private List<Op> getOps() {
+      return Collections.unmodifiableList(ops);
+    }
+  }
+  
+  private static class Op {
+    private final Score parent;
+    private final String from;
+    private final String reason;
+    private final boolean isIncrement;
+    
+    public Op(Score parent, String from, String reason, boolean isIncrement) {
+      this.parent = parent;
+      this.from = from;
+      this.reason = reason == null || reason.trim().length() == 0 
+                    ? null
+                    : reason;
+      this.isIncrement = isIncrement;
+    }
+    
+    private Message getResponseMessage() {
+      String format = isIncrement ? INC_MESSAGE_FORMAT : DEC_MESSAGE_FORMAT;
+      String formattedReason = getFormattedReason();
+      String result = String.format(format, 
+                                    parent.getTarget(),
+                                    parent.getCurrentScore(),
+                                    formattedReason);
+      
+      return new Message(null, null, result);
+    }
+    
+    private String getFormattedReason() {
+      return reason != null 
+             ? String.format(REASON_FORMAT, reason)
+             : "";    
+    }
+    
+    private String getDescription() {
+      String type = isIncrement ? "increment" : "decrement";
+      String formattedReason = getFormattedReason();    
+      return String.format(OP_DESCRIPTION_FORMAT, type, from, formattedReason);
+    }
+  }
 
   private BufferedWriter logFile;
   
   public PlusPlusBot() {
     blacklistedTargets = new HashSet<String>();
-    scoreBoard = new HashMap<String, Map<String, Integer>>();
+    scoreBoard = new HashMap<String, Map<String, Score>>();
     try {
       logFile = prepareAndLoadLog("ppblog");
     } catch (IOException e) {
@@ -67,13 +156,14 @@ public class PlusPlusBot {
    * We're too lazy to write proper tests. Use this instead. ;-)
    */
   public static void main(String[] args) {
-    Matcher matcher = Pattern.compile("(\\S+)(\\+\\+|\\-\\-)\\W*(\\w*.*)").matcher("cows++");
+    Matcher matcher = 
+        Pattern.compile("(\\S+)(\\+\\+|\\-\\-)\\W*(\\w*.*)").matcher("cows++");
     matcher.find();
     System.err.println(matcher.group(1));
     PlusPlusBot ppb = new PlusPlusBot();
-    System.out.println(ppb.getScores("chat", null));
+    System.out.println(ppb.getScores("chat", null, false));
     ppb.doCommand("sender", "chat", "target", "reason", true);
-    System.out.println(ppb.getScores("chat", null));
+    System.out.println(ppb.getScores("chat", null, false));
     try {
       ppb.logFile.flush();
     } catch (IOException e) {
@@ -120,17 +210,12 @@ public class PlusPlusBot {
   }
 
 
-  public Message doCommand(String from, String chat, String target, String reason,
-      boolean increment) {
+  public Message doCommand(String from, String chat, String target,
+      String reason, boolean increment) {
     
     // Skip the blacklist
     if (blacklistedTargets.contains(target)) {
       return null;
-    }
-    
-    // For old time's sake, also increment the letter associated with this action
-    if (target.length() > 1) {
-      getUpdatedScore(chat, target.substring(target.length() - 1), increment);
     }
     
     // Log the event
@@ -140,9 +225,8 @@ public class PlusPlusBot {
       e.printStackTrace();
     }
     
-    // Update the map
-    int currentScore = getUpdatedScore(chat, target, increment);
-    return getResponseMessage(target, reason, increment, currentScore);
+    // Update the map and generate a response message
+    return updateScore(from, chat, target, reason, increment);
   }
 
   private void logEvent(String from, String chat, String target, String reason,
@@ -160,84 +244,73 @@ public class PlusPlusBot {
     logFile.flush();
   }
 
-  private Message getResponseMessage(String target, String reason,
-      boolean increment, int currentScore) {
-    String formattedReason = getReasonString(reason);
-    String format = increment ? INC_MESSAGE_FORMAT : DEC_MESSAGE_FORMAT;
-    String result = String.format(format, target, currentScore, formattedReason);
-    Message m = new Message(null, null, result);
-    return m;
-  }
+  private Message updateScore(String from, String chat, String target, 
+      String reason, boolean increment) {
+    Map<String, Score> scores = getScoreBoard(chat);
 
-
-  private int getUpdatedScore(String chat, String target, boolean increment) {
-    int currentScore = 0;
-    Map<String, Integer> scores = getScoreBoard(chat);
-
-    if (scores.containsKey(target)) {
-      currentScore = scores.get(target);
+    Score score = scores.get(target);
+    if (score == null) {
+      score = new Score(target);
+      scores.put(target, score);
     }
     
-    currentScore = increment ? currentScore + 1 : currentScore - 1;
-    scores.put(target, currentScore);
-    return currentScore;
+    if (increment) {
+      return score.increment(from, reason);
+    } else {
+      return score.decrement(from, reason);
+    }
   }
   
-  private synchronized Map<String, Integer> getScoreBoard(String chat) {
+  private synchronized Map<String, Score> getScoreBoard(String chat) {
     if (scoreBoard.containsKey(chat)) {
       return scoreBoard.get(chat);
     }
-    Map<String, Integer> scores = Collections.synchronizedMap(
-        new HashMap<String, Integer>());
+    Map<String, Score> scores = Collections.synchronizedMap(
+        new HashMap<String, Score>());
     scoreBoard.put(chat, scores);
     return scores;
   }
   
-  private String getReasonString(String reason) {
-    if (reason == null || reason.trim().length() == 0) {
-      return "";
-    }
-    return reason;
-  }
-
-
-  public String getScores(String chat, String regex) {
-    List<Entry<String, Integer>> toShow =
-      new ArrayList<Entry<String, Integer>>();
-    
-    Set<Entry<String, Integer>> allScores = getScoreBoard(chat).entrySet();
+  public String getScores(String chat, String regex, boolean showReasons) {
+    List<Score> scoresToShow = new ArrayList<Score>();
+    Collection<Score> allScores = getScoreBoard(chat).values();
     
     if (regex != null && !regex.equals("")) {
       Pattern searchPattern;
       try {
         searchPattern = Pattern.compile(regex);
-      } catch (RuntimeException e) {
+      } catch (PatternSyntaxException e) {
         return "invalid pattern";
       }
       
-      for (Entry<String, Integer> elt : allScores) {
-        if (searchPattern.matcher(elt.getKey()).matches()) {
-          toShow.add(elt);
+      for (Score score : allScores) {
+        if (searchPattern.matcher(score.getTarget()).matches()) {
+          scoresToShow.add(score);
         }
       }  
     } else {
-      toShow.addAll(allScores);
-      Collections.sort(toShow, new Comparator<Entry<String, Integer>>() {
-        public int compare(Entry<String, Integer> a,
-            Entry<String, Integer> b) {
-          return a.getValue().compareTo(b.getValue());
-        }
-      });
+      scoresToShow.addAll(allScores);
     }
     
+    // Sort entries by score value
+    Collections.sort(scoresToShow);    
+    
+    // Render response
     StringBuilder result = new StringBuilder();
-    for (Entry<String, Integer> elt : toShow) {
+    for (Score score : scoresToShow) {
       if (result.length() > 0) {
         result.append("\n");
       }
-      result.append(elt.getKey())
+      result.append(score.getTarget())
             .append(":")
-            .append(elt.getValue());
+            .append(score.getCurrentScore());
+
+      if (showReasons) {
+        for (Op op : score.getOps()) {
+          result.append("\n");
+          result.append(op.getDescription());
+        }
+      }
     }
     
     if (result.length() == 0) {
