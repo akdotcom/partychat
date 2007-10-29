8c68bf2cea7235d30a19abc750f1d09465e5e11d
mihaiparparita <mihaiparparita@063ddc40-722a-11de-b54e-29fd3b018196>
2007-10-29 12:53:23 +0000

    Clean up /list command and make it show idle time and bot that is used.
    Fix TODO in StatsCommandHandler.java.

--- trunk/partychat/src/net/q00p/bots/partybot/ListCommandHandler.java
+++ trunk/partychat/src/net/q00p/bots/partybot/ListCommandHandler.java
@@ -2,32 +2,39 @@ package net.q00p.bots.partybot;
 
 import java.util.regex.Matcher;
 
-public class ListCommandHandler implements CommandHandler {
+public class ListCommandHandler extends PartyLineCommandHandler {
 
-  public String doCommand(PartyBot partyBot, LineManager lineManager,
-      Subscriber subscriber, Matcher commandMatcher) {
-    PartyLine partyLine = lineManager.getPartyLine(subscriber);
-    if (partyLine == null)
-      return LineManager.NOT_IN;
-    else
-      return formatSubscriberList(partyLine);
-  }
-
-  private String formatSubscriberList(PartyLine partyLine) {
+  public String doCommand(
+      PartyBot partyBot,
+      PartyLine partyLine,
+      Subscriber subscriber,
+      Matcher commandMatcher) {
     StringBuilder sb = new StringBuilder();
     
     sb.append("#" + partyLine.getName() + " members:\n");
+
     for (Subscriber sub : partyLine.getSubscribers()) {
       sb.append(sub.getUser().getName());
 
-      if (sub.getAlias() != null) sb.append(" (" + sub.getAlias() + ")");
-      
-      if (sub.isSnoozing()) {
-        sb.append(" snoozing for " + PartyBot.timeTill(sub.getSnoozeUntil()));
+      if (sub.getAlias() != null) {
+        sb.append(" (")
+          .append(sub.getAlias())
+          .append(")");
       }
       
-      sb.append(" ");
-      sb.append(sub.getUser().getClientInfo());
+      if (sub.isSnoozing()) {
+        sb.append(" snoozing for ")
+          .append(PartyBot.timeTill(sub.getSnoozeUntil()));
+      }
+      
+      sb.append(" using ")
+        .append(sub.getBotScreenName().toLowerCase());
+
+      if (sub.getLastActivityTime() > 0) {
+        sb.append(" last seen ")
+          .append(PartyBot.timeSince(sub.getLastActivityTime()))
+          .append(" ago");
+      }
       
       sb.append("\n");
     }
