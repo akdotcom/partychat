a6f97826a6d445eb5185ad2566b3b993c1e1abc6
mihaiparparita <mihaiparparita@063ddc40-722a-11de-b54e-29fd3b018196>
2008-03-15 22:00:47 +0000

    When someone leaves, also include how many instances are left in the room.

--- partychat/src/net/q00p/bots/partybot/ExitCommandHandler.java
+++ partychat/src/net/q00p/bots/partybot/ExitCommandHandler.java
@@ -5,22 +5,41 @@ import java.util.regex.Matcher;
 public class ExitCommandHandler implements CommandHandler {
 
   private static final String SUB_LEFT = "%s left the chat";
+  private static final String ONE_INSTANCE_LEFT = "%s instance remains";
+  private static final String MANY_INSTANCE_LEFT = "%s instances remain";
   
   public String doCommand(PartyBot partyBot, LineManager lineManager,
       Subscriber subscriber, Matcher commandMatcher) {
     Subscriber.forget(subscriber);
     
-    String name = subscriber.getUser().getName();
-    
-    if (subscriber.getAlias() != null) {
-      name += " (" + subscriber.getAlias() + ")";
-    }
-    
-    String announcement = String.format(SUB_LEFT, name);
-    
     PartyLine partyLine = lineManager.getPartyLine(subscriber);
 
     if (partyLine != null) {
+      String name = subscriber.getUser().getName();
+      
+      if (subscriber.getAlias() != null) {
+        name += " (" + subscriber.getAlias() + ")";
+      }
+
+      String announcement = String.format(SUB_LEFT, name);
+      
+      // Count how many instances of this user remain, in case they were in
+      // the room multiple times
+      int instanceCount = 0;
+      for (Subscriber sub : partyLine.getSubscribers()) {
+        if (sub != subscriber &&
+            sub.getUser().getName().equals(subscriber.getUser().getName())) {
+          instanceCount++;
+        }
+      }
+      
+      if (instanceCount > 0) {
+        announcement += ", " + String.format(
+          instanceCount == 1 ? ONE_INSTANCE_LEFT : MANY_INSTANCE_LEFT, 
+          instanceCount);
+      }
+      
+
       partyBot.announce(partyLine, announcement);
     }
     
