b983004663ca0628286c487797483995d4c41c7e
mihaiparparita <mihaiparparita@063ddc40-722a-11de-b54e-29fd3b018196>
2008-03-15 21:47:12 +0000

    When leaving a party chat, also display the alias.

--- trunk/partychat/src/net/q00p/bots/partybot/ExitCommandHandler.java
+++ trunk/partychat/src/net/q00p/bots/partybot/ExitCommandHandler.java
@@ -9,13 +9,21 @@ public class ExitCommandHandler implements CommandHandler {
   public String doCommand(PartyBot partyBot, LineManager lineManager,
       Subscriber subscriber, Matcher commandMatcher) {
     Subscriber.forget(subscriber);
-    String announcement = String.format(SUB_LEFT, subscriber.getUser()
-        .getName());
+    
+    String name = subscriber.getUser().getName();
+    
+    if (subscriber.getAlias() != null) {
+      name += " (" + subscriber.getAlias() + ")";
+    }
+    
+    String announcement = String.format(SUB_LEFT, name);
+    
     PartyLine partyLine = lineManager.getPartyLine(subscriber);
 
     if (partyLine != null) {
       partyBot.announce(partyLine, announcement);
     }
+    
     return lineManager.unsubscribe(subscriber, commandMatcher.group(2));
   }
 
