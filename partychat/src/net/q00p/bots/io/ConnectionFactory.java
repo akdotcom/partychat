623605d3ff37ed011d10e7cd9fce31c6df35da00
ak_ <ak_@063ddc40-722a-11de-b54e-29fd3b018196>
2009-07-16 13:41:45 +0000

    Make connection security clearer, make explicit some defaults and clean up domain/server extraction logic.

--- partychat/src/net/q00p/bots/io/ConnectionFactory.java
+++ partychat/src/net/q00p/bots/io/ConnectionFactory.java
@@ -1,6 +1,7 @@
 package net.q00p.bots.io;
 
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.provider.ProviderManager;
@@ -12,9 +13,9 @@ public class ConnectionFactory {
 	private static final String DEFAULT_DOMAIN = "gmail.com";
 	
 	public static void init() {
-    // Don't want offline messages
-		ProviderManager.getInstance().
-		    removeExtensionProvider("x", "jabber:x:delay");	
+	    // Don't want offline messages
+	    ProviderManager.getInstance().
+		removeExtensionProvider("x", "jabber:x:delay");	
 	}
 	
 	public static Connection getConnection(String username, String password) {
@@ -23,23 +24,34 @@ public class ConnectionFactory {
 	  
 	  int atIndex = username.indexOf("@");
 	  if (atIndex != -1) {
-	    server = domain = username.substring(atIndex + 1);
+	    domain = username.substring(atIndex + 1);
+	    // google hosted domains talk to talk.google.com servers. most other domains talk to their
+	    // own server.
+	    if (!domain.equals("gmail.com")) {
+		server = domain;
+	    }
 	    username = username.substring(0, atIndex);
 	  }
+	  // Create the configuration for the connection
+	  ConnectionConfiguration config = new ConnectionConfiguration(server, 5222, domain);
+	  //	  config.setCompressionEnabled(true);
+	  config.setSASLAuthenticationEnabled(false);
 	  
-		XMPPConnection connection;
-		try {
-			connection = new XMPPConnection(
-			    new ConnectionConfiguration(server, 5222, domain));
-			connection.connect();
-			connection.login(username, password);
-		} catch (XMPPException e) {
-			throw new GoogleTalkException(e);
-		}
-		Connection conn = new Connection(connection);
-		cm.addConnection(username + "@" + domain, conn);
-
-		return conn;
+	  XMPPConnection connection;
+	  try {
+	      connection = new XMPPConnection(config);
+	      //	    new ConnectionConfiguration(server, 5222, domain));
+	      connection.connect();
+	      connection.login(username, password);
+	  } catch (XMPPException e) {
+	      throw new GoogleTalkException(e);
+	  }
+	  Roster roster = connection.getRoster();
+	  roster.setSubscriptionMode(Roster.SubscriptionMode.accept_all);
+	  Connection conn = new Connection(connection);
+	  cm.addConnection(username + "@" + domain, conn);
+	  
+	  return conn;
 	}
 	
 	public static ConnectionManager getConnectionManager() {
