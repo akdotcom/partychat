17f538fa2027a59c171595be9e028cd1d614d675
mihai.parparita <mihai.parparita@063ddc40-722a-11de-b54e-29fd3b018196>
2009-08-18 17:55:05 +0000

    Group /list output by subscriber name.

--- trunk/partychat/src/net/q00p/bots/partybot/ListCommandHandler.java
+++ trunk/partychat/src/net/q00p/bots/partybot/ListCommandHandler.java
@@ -1,5 +1,7 @@
 package net.q00p.bots.partybot;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import java.util.regex.Matcher;
 
 public class ListCommandHandler extends PartyLineCommandHandler {
@@ -13,31 +15,38 @@ public class ListCommandHandler extends PartyLineCommandHandler {
     StringBuilder sb = new StringBuilder();
     
     sb.append("#" + partyLine.getName() + " members:\n");
-
+    
+    Multimap<String, Subscriber> subscribersByName = HashMultimap.create();
+    
     for (Subscriber sub : partyLine.getSubscribers()) {
-      sb.append(sub.getUser().getName());
+      subscribersByName.put(sub.getUser().getName(), sub);
+    }
+    
+    for (String name : subscribersByName.keySet()) {
+      sb.append(name).append(":\n");
+      
+      for (Subscriber sub : subscribersByName.get(name)) {
+        sb.append("  using ")
+          .append(sub.getBotScreenName().toLowerCase());
 
-      if (sub.getAlias() != null) {
-        sb.append(" (")
-          .append(sub.getAlias())
-          .append(")");
-      }
-      
-      if (sub.isSnoozing()) {
-        sb.append(" snoozing for ")
-          .append(PartyBot.timeTill(sub.getSnoozeUntil()));
-      }
-      
-      sb.append(" using ")
-        .append(sub.getBotScreenName().toLowerCase());
+        if (sub.getAlias() != null) {
+          sb.append(" as ").append(sub.getAlias());
+        }
 
-      if (sub.getLastActivityTime() > 0) {
-        sb.append(" last seen ")
-          .append(PartyBot.timeSince(sub.getLastActivityTime()))
-          .append(" ago");
+        if (sub.isSnoozing()) {
+          sb.append(" snoozing for ")
+            .append(PartyBot.timeTill(sub.getSnoozeUntil()));
+        }
+
+
+        if (sub.getLastActivityTime() > 0) {
+          sb.append(" last seen ")
+            .append(PartyBot.timeSince(sub.getLastActivityTime()))
+            .append(" ago");
+        }
+        
+        sb.append("\n");
       }
-      
-      sb.append("\n");
     }
     
     return sb.toString();
